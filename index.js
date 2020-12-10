04402bf2b760a0137924990988a64f567a60f9be
Shubham-503 <62199573+Shubham-503@users.noreply.github.com>
2020-12-10 18:50:08 +0530

    Remove: spinner

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ let txtarea = document.querySelector("#textarea");
 let btnTranslate = document.querySelector("#btn-translate");
 let dothrakiText = document.querySelector("#dothraki-text");
 
-const spinner = document.getElementById("spinner");
-
-function showSpinner() {
-  spinner.className = "show";
-  setTimeout(() => {
-    spinner.className = spinner.className.replace("show", "");
-  }, 500);
-}
-
 function fetchUrl(text) {
   return "https://api.funtranslations.com/translate/dothraki.json?text=" + text;
 }
@@ -23,7 +14,6 @@ function errorHandler(error) {
 function handleTranslate() {
   //   console.log("translated");
   text = txtarea.value;
-  showSpinner();
   fetch(fetchUrl(text))
     .then((response) => response.json())
     .then((json) => (dothrakiText.innerText = json.contents.translated))
